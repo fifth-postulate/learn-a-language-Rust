3007cd78325986b4d475a973a3816a4ceac08242
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2016-02-08 14:20:13 +0100

    Create a fibonacci code sample

---
+++ code-samples/fibonacci.rs
@@ -0,0 +1,15 @@
+fn fib(n: i64) -> i64 {
+    if n == 0 {
+        0
+    } else if n == 1 {
+        1
+    } else {
+        fib(n - 1) + fib(n - 2)
+    }
+}
+
+fn main() {
+    let argument = 10;
+
+    println!("fib({}) = {}", argument, fib(argument));
+}
